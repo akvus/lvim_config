bc20e6d909ab3071e97ab1f9db6a0bf6eee8757f
akvus <mr.akvus@gmail.com>
2023-03-27 18:20:41 +0200

    Remap copilot accept to C-w

--- config.lua
+++ config.lua
@@ -419,7 +419,7 @@ lvim.plugins = {
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
-lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
+lvim.builtin.cmp.mapping["<C-w>"] = function(fallback)
   local copilot_keys = vim.fn["copilot#Accept"]()
   if copilot_keys ~= "" then
     vim.api.nvim_feedkeys(copilot_keys, "i", true)
--- luasnippets/dart.lua
+++ luasnippets/dart.lua
@@ -1,8 +1,8 @@
 return {
   s("testfile", { 
-    t "import 'package:flutter_test/flutter_test.dart';" ,
-    t "import 'package:mocktail/mocktail.dart';",
-    t "",
-    t "void main() {group('', () {});}",
+    t "import 'package:flutter_test/flutter_test.dart';/n" ,
+    t "import 'package:mocktail/mocktail.dart/n';",
+    t "/n",
+    t "void main() {group('', () {});}/n",
   }),
 }
