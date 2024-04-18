ecd93d7e436180ebc81392d246c783b414da68db
akvus <mr.akvus@gmail.com>
2024-04-18 12:19:40 +0200

    Move things related to powershell out of the main config into a file

--- config.lua
+++ config.lua
@@ -11,34 +11,7 @@ C:\Users\conta\AppData\Local\lvim
 C:\Users\conta\AppData\Roaming\lunarvim
 ]]
 
-function IsWindows()
-  return vim.loop.os_uname().sysname == 'Windows_NT';
-end
-
-if (IsWindows()) then
-  -- Defaults that came with LunarVim 1.3
-  vim.opt.shell = "pwsh.exe -NoLogo"
-  vim.opt.shellcmdflag =
-  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-  vim.cmd [[
-      let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-      let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-      set shellquote= shellxquote=
-  ]]
-
-  -- Set a compatible clipboard manager
-  vim.g.clipboard = {
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-  }
-end
-
+require "powershell"
 
 -- SETTINGS
 lvim.log.level = "warn"
