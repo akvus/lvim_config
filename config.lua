5fc934a35b550306b9c6c9c034d05d09b0f1ee83
akvus <zawieja.mobile@gmail.com>
2024-05-23 15:02:11 +0200

    Add harpoon2

--- config.lua
+++ config.lua
@@ -298,6 +298,30 @@ lvim.plugins = {
     "mg979/vim-visual-multi",
   },
   {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-S-J>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<C-S-K>", function() harpoon:list():next() end)
+    end,
+  },
+  {
     "nvim-neotest/neotest",
     dependencies = {
       'sidlatau/neotest-dart',
