4e5c106d9281d69912967ae8e7fb63be39d3db8a
akvus <zawieja.mobile@gmail.com>
2024-07-02 16:02:42 +0200

    Add some basic mappings for gradle

--- lua/java.lua
+++ lua/java.lua
@@ -30,3 +30,11 @@ lvim.builtin.which_key.mappings["m"] = {
   t = { "<cmd>ter " .. mvnCommand .. " test<cr>", "Test" },
   T = { "<cmd>ter " .. mvnCommand .. " spring-boot:test-run<cr>", "Test run spring" },
 }
+
+-- Gradle
+lvim.builtin.which_key.mappings["r"] = {
+  r = { "<cmd>ter gradle bootRun<cr>", "Run spring boot with gradle" },
+  c = { "<cmd>ter gradle clean<cr>", "Clean" },
+  t = { "<cmd>ter gradle test<cr>", "Test" },
+  T = { "<cmd>ter gradle test<cr>", "Test" },
+}
