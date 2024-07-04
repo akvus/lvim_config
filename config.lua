4ce3e53c8ce69490dd953094e4b0fa99993c643e
akvus <zawieja.mobile@gmail.com>
2024-07-04 20:38:39 +0200

    Remove trouble

--- config.lua
+++ config.lua
@@ -10,6 +10,8 @@ require "powershell"
 require "flutter"
 require "java"
 
+require 'lspconfig'.kotlin_language_server.setup{}
+
 -- SETTINGS
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
@@ -158,15 +160,6 @@ lvim.builtin.which_key.mappings["W"] = {
   c = { "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>", "Create" },
 }
 
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  -- TODO: do I still need trouble? revmoe in favor of telescope + quickfix?
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-}
-
 lvim.builtin.which_key.mappings["n"] = {
   name = "+neotest",
   n = { "<cmd>lua require('neotest').run.run()<cr>", "Nearest" },
@@ -188,6 +181,7 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
   "java",
+  "kotlin",
   "javascript",
   "json",
   "lua",
@@ -195,7 +189,6 @@ lvim.builtin.treesitter.ensure_installed = {
   "tsx",
   "yaml",
   "dart",
-  "kotlin",
   "markdown",
   "markdown_inline",
   "python",
@@ -208,6 +201,7 @@ lvim.builtin.treesitter.highlight.enable = true
 lvim.lsp.installer.setup.ensure_installed = {
   "jsonls",
   "tsserver",
+  "kotlin_language_server",
 }
 
 ---@usage disable automatic installation of servers
@@ -236,63 +230,6 @@ lvim.plugins = {
     config = function()
       require('log-highlight').setup {}
     end,
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      position = "bottom",            -- position of the list can be: bottom, top, left, right
-      height = 10,                    -- height of the trouble list when position is top or bottom
-      width = 50,                     -- width of the list when position is left or right
-      icons = true,                   -- use devicons for filenames
-      mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-      severity = nil,                 -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
-      cycle_results = true,           -- cycle item list when reaching beginning or end of list
-      action_keys = {
-        -- key mappings for actions in the trouble list
-        -- map to {} to remove a mapping, for example:
-        -- close = {},
-        close = "q",                                                                        -- close the list
-        cancel = "<esc>",                                                                   -- cancel the preview and get back to your last window / buffer / cursor
-        refresh = "r",                                                                      -- manually refresh
-        jump = { "<cr>", "<tab>", "<2-leftmouse>" },                                        -- jump to the diagnostic or open / close folds
-        open_split = { "<c-x>" },                                                           -- open buffer in new split
-        open_vsplit = { "<c-v>" },                                                          -- open buffer in new vsplit
-        open_tab = { "<c-t>" },                                                             -- open buffer in new tab
-        jump_close = { "o" },                                                               -- jump to the diagnostic and close the list
-        toggle_mode = "m",                                                                  -- toggle between "workspace" and "document" diagnostics mode
-        switch_severity = "s",                                                              -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
-        toggle_preview = "P",                                                               -- toggle auto_preview
-        hover = "K",                                                                        -- opens a small popup with the full multiline message
-        preview = "p",                                                                      -- preview the diagnostic location
-        open_code_href = "c",                                                               -- if present, open a URI with more information about the diagnostic error
-        close_folds = { "zM", "zm" },                                                       -- close all folds
-        open_folds = { "zR", "zr" },                                                        -- open all folds
-        toggle_fold = { "zA", "za" },                                                       -- toggle fold of current file
-        previous = "k",                                                                     -- previous item
-        next = "j",                                                                         -- next item
-        help = "?"                                                                          -- help menu
-      },
-      multiline = true,                                                                     -- render multi-line messages
-      indent_lines = true,                                                                  -- add an indent guide below the fold icons
-      win_config = { border = "single" },                                                   -- window configuration for floating windows. See |nvim_open_win()|.
-      auto_open = false,                                                                    -- automatically open the list when you have diagnostics
-      auto_close = true,                                                                    -- automatically close the list when you have no diagnostics
-      auto_preview = true,                                                                  -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-      auto_fold = false,                                                                    -- automatically fold a file trouble list at creation
-      auto_jump = { "lsp_definitions" },                                                    -- for the given modes, automatically jump if there is only a single result
-      include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions" }, -- for the given modes, include the declaration of the current symbol in the results
-      signs = {
-        -- icons / text used for a diagnostic
-        error = "",
-        warning = "",
-        hint = "",
-        information = "",
-        other = "",
-      },
-      use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
-    },
-    cmd = "TroubleToggle",
   },
   {
     "mg979/vim-visual-multi",
